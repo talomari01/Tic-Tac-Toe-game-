e6a261dc6f07de99567cfc4377597b0fb2ab4f4a
Turki Alomari <t-matar@windowslive.com>
2018-12-01 21:07:55 +0300

    style.css

--- main.js
+++ main.js
@@ -1,1 +1,88 @@
-console.log("turki")
+
+ 
+ 
+
+$(document).ready(function(){
+
+    var boxs = $(".box") //selected box class from index.html 
+    var turn = true 
+    var x ='x', o = "o";
+    
+
+    game();
+
+    function game(){
+        boxs.on('click',function(){
+            if (turn && $(this).html() == ""){
+                $(this).html(o);
+                turn = false; 
+               
+              
+                selectWinner();
+                
+            }else if (!turn && $(this).html() == "") {
+                $(this).html(x);
+                turn = true;
+                selectWinner();
+                ``
+            } 
+            
+        })
+        
+    }
+
+    function selectWinner(){
+
+         var winnerArry = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [2,4,6],
+            [0,4,8]
+         ]
+    
+         for(var i = 0; i < winnerArry.length; i++){
+             var one = winnerArry[i][0];
+             console.log('one = ',one);
+             var two = winnerArry[i][1];
+             console.log('two',two);
+             var three = winnerArry[i][2];
+             console.log('three',three);
+
+             //console.log($(boxs[one]).html());
+             if ( $(boxs[one]).html() == $(boxs[two]).html() && $(boxs[two]).html() == $(boxs[three]).html() && $(boxs[three]).html() !== ''){
+                 winner($(boxs[one]).html());
+             }
+         }
+         if ( $(boxs[0]).html() && $(boxs[1]).html() && $(boxs[2]).html() && $(boxs[3]).html() && $(boxs[4]).html() && $(boxs[5]).html() && $(boxs[6]).html() && $(boxs[7]).html() && $(boxs[8]).html() ){
+            alert ("draw");
+         }
+
+    }
+
+    function winner(plyer){
+        if (plyer == 'o'){
+            alert ("The winner is O")
+
+        } else {
+            alert('The winner is x')
+        }
+    }
+
+
+        
+        
+$("#rest").on('click', function(){
+    
+        boxs.html("");
+    
+
+})
+
+
+
+
+})
--- read.md
+++ read.md
@@ -1,1 +1,16 @@
 #tic-tac
+#summary of tic tac game:
+The game is played on grid thats 3 squares by 3 aquares, and there are two character (O) (X).
+Players take turns putting their marks in empty squares.
+The first player to get 3 of marks in a row (up,down, across) is the winner.
+If no player has 3 marks in a row, the game ends in a tie.
+# technolgies used :
+html / css / JQ
+
+#things to add:
+play with the computer 
+name of players
+
+#Resources 
+https://api.jquery.com/
+https://www.w3schools.com
